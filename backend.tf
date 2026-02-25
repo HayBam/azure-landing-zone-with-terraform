9a8b6995ea50a78c9a3c70dd829eab6a018637b9
HayBam <ayobami.odunlami@hotmail.com>
2026-02-25 06:17:13 -0500

    Updated resource group name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "landing-zone-terraform-rg"
+    resource_group_name  = "landing-zone-terraform-resource-group"
     storage_account_name = "storageterraformstateayo"
     container_name       = "tfstate"
     key                  = "landing-zone.tfstate"
