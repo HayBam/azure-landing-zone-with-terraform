36e99445ae4fd808c4537b093e94a401871ef397
HayBam <ayobami.odunlami@hotmail.com>
2026-02-26 20:50:39 -0500

    Added resource for subscription and hub and spoke modules

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 # -----------------------------------------------
+# Subscription Association
+# Moves the subscription from Tenant Root Group into our custom root MG 
+# -----------------------------------------------
+resource "azurerm_management_group_subscription_association" "root" {
+  management_group_id = module.management_groups.root_mg_id
+  subscription_id     = "/subscriptions/${var.subscription_id}"
+}
+
+# -----------------------------------------------
 # Management Group Hierarchy
 # -----------------------------------------------
 module "management_groups" {
@@ -17,5 +26,47 @@ module "hub_network" {
   vnet_name           = "vnet-hub"
   address_space       = var.hub_vnet_address_space
   tags                = var.tags
-  deploy_firewall     = false  # Set to true only when testing firewall rules
+  deploy_firewall     = false  # Set to true when testing firewall rules
+
+  depends_on = [azurerm_management_group_subscription_association.root]
+}
+
+# -----------------------------------------------
+# Dev Spoke Network
+# -----------------------------------------------
+module "dev_spoke" {
+  source                  = "./modules/spoke-network"
+  resource_group_name     = "rg-spoke-dev"
+  location                = var.location
+  vnet_name               = "vnet-spoke-dev"
+  address_space           = var.dev_spoke_address_space
+  workload_subnet_prefix  = "10.1.1.0/24"
+  hub_vnet_id             = module.hub_network.hub_vnet_id
+  hub_vnet_name           = module.hub_network.hub_vnet_name
+  hub_resource_group_name = module.hub_network.hub_resource_group_name
+  route_table_id          = module.hub_network.route_table_id != null ? module.hub_network.route_table_id : ""
+  tags                    = var.tags
+  environment             = "dev"
+
+  depends_on = [azurerm_management_group_subscription_association.root]
+}
+
+# -----------------------------------------------
+# Prod Spoke Network
+# -----------------------------------------------
+module "prod_spoke" {
+  source                  = "./modules/spoke-network"
+  resource_group_name     = "rg-spoke-prod"
+  location                = var.location
+  vnet_name               = "vnet-spoke-prod"
+  address_space           = var.prod_spoke_address_space
+  workload_subnet_prefix  = "10.2.1.0/24"
+  hub_vnet_id             = module.hub_network.hub_vnet_id
+  hub_vnet_name           = module.hub_network.hub_vnet_name
+  hub_resource_group_name = module.hub_network.hub_resource_group_name
+  route_table_id          = module.hub_network.route_table_id != null ? module.hub_network.route_table_id : ""
+  tags                    = var.tags
+  environment             = "prod"
+
+  depends_on = [azurerm_management_group_subscription_association.root]
 }
